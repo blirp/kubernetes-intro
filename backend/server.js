d7f32ffbdb70ced2b1ba8eb37591550c28065128
Martin Larsson <martin.larsson@delfidata.no>
2018-12-12 19:17:38 +0100

    Mine data

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Martin Larsson", "location": "Bergen"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "1996", "yearTo": "2017", "place": "Delfi Data", "comment": "Coding"}, {"yearFrom": "2017", "yearTo": "2018", "place": "Stacc Insight", "comment": "Architecting"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "1989", "yearTo": "1992", "place": "BIH", "comment": "Studying Computer Science"}, {"yearFrom": "1994", "yearTo": "1995", "place": "CU", "comment": "More CS"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
